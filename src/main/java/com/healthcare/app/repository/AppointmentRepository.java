532f74bb5736a3a2cd790231ef1343aec2f15b82
PRABHAT KASHYAP <67687407+PRABHATKASHYA@users.noreply.github.com>
2026-03-16 16:21:21 +0530

    updated the csv file.and Tests

--- src/main/java/com/healthcare/app/repository/AppointmentRepository.java
+++ src/main/java/com/healthcare/app/repository/AppointmentRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.healthcare.app.entity.Appointment;
 
+import java.util.List;
+
 public interface AppointmentRepository  extends JpaRepository<Appointment, Long> {
 
+    List <Appointment>searchBypatientId(String patientId);
 }
